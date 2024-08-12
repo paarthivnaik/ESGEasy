913e78e02575803cae5e7dc5af9eea5cf7a77ec8
Balaji Naik Mudavatu <b.mudavatu@eurasiansoft.com>
2024-08-12 17:27:10 +0530

    GGG

--- ESG.Domain/Common/AuditEntry.cs
+++ ESG.Domain/Common/AuditEntry.cs
@@ -1,10 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+﻿using ESG.Domain.Entities;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ESG.Domain.Common
 {
--- ESG.Domain/Entities/Audit.cs
+++ ESG.Domain/Entities/Audit.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ESG.Domain.Common;
 
-namespace ESG.Domain.Common
+namespace ESG.Domain.Entities
 {
-    public class Audit:BaseEntity<long>
+    public class Audit : BaseEntity<long>
     {
         public long CreatedBy { get; set; }
         public string Type { get; set; }
@@ -16,6 +17,6 @@ namespace ESG.Domain.Common
         public string NewValues { get; set; }
         public string? AffectedColumns { get; set; }
         public string PrimaryKey { get; set; }
-       
+
     }
 }
